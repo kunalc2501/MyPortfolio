a7c6af7c2c1c02511562db4d5944739b63e3587c
Kunal Choudhary <kunalc2501@gmail.com>
2026-01-24 17:16:40 +0530

    upadate

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-let navmenulinks = document.querySelectorAll('.navmenu a');
+let navmenulinks = document.querySelectorAll(".navmenu a");
 
-        function navmenuScrollspy() {
-            navmenulinks.forEach(navmenulink => {
-                if (!navmenulink.hash) return;
-                let section = document.querySelector(navmenulink.hash);
-                if (!section) return;
-                let position = window.scrollY + 200;
-                if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-                    document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
-                    navmenulink.classList.add('active');
-                } else {
-                    navmenulink.classList.remove('active');
-                }
-            })
-        }
-        window.addEventListener('load', navmenuScrollspy);
-        document.addEventListener('scroll', navmenuScrollspy);
+function navmenuScrollspy() {
+  navmenulinks.forEach((navmenulink) => {
+    if (!navmenulink.hash) return;
+    let section = document.querySelector(navmenulink.hash);
+    if (!section) return;
+    let position = window.scrollY + 200;
+    if (
+      position >= section.offsetTop &&
+      position <= section.offsetTop + section.offsetHeight
+    ) {
+      document
+        .querySelectorAll(".navmenu a.active")
+        .forEach((link) => link.classList.remove("active"));
+      navmenulink.classList.add("active");
+    } else {
+      navmenulink.classList.remove("active");
+    }
+  });
+}
+window.addEventListener("load", navmenuScrollspy);
+document.addEventListener("scroll", navmenuScrollspy);
 
+const dob = new Date("1999-10-01"); // Change this to your DOB
 
-        const dob = new Date("1999-10-01");  // Change this to your DOB
+const today = new Date();
+let age = today.getFullYear() - dob.getFullYear();
+const m = today.getMonth() - dob.getMonth();
 
-        const today = new Date();
-        let age = today.getFullYear() - dob.getFullYear();
-        const m = today.getMonth() - dob.getMonth();
+if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
+  age--;
+}
 
-        if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
-            age--;
-        }
+document.getElementById("age").textContent = age;
 
-        document.getElementById("age").textContent = age;
-        
+// const navLinks = document.querySelectorAll(".navmenu a");
+// const offcanvasEl = document.getElementById("offcanvasNavbar2");
+
+// const bsOffcanvas = bootstrap.Offcanvas.getOrCreateInstance(offcanvasEl);
+
+// navLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     if (window.innerWidth < 992) {
+//       bsOffcanvas.hide();
+//     }
+//   });
+// });
