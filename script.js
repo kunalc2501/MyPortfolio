e5971e5ce2a64b250f47b65899de7dc28d33cafa
Kunal Choudhary <kunalc2501@gmail.com>
2026-01-31 14:01:13 +0530

    contact section worked complete from section

--- script.js
+++ script.js
@@ -22,17 +22,52 @@ function navmenuScrollspy() {
 window.addEventListener("load", navmenuScrollspy);
 document.addEventListener("scroll", navmenuScrollspy);
 
-const dob = new Date("1999-10-01"); // Change this to your DOB
+//=============== start date code =======================//
 
-const today = new Date();
-let age = today.getFullYear() - dob.getFullYear();
-const m = today.getMonth() - dob.getMonth();
+// const dob = new Date("01-10-1999"); // Change this to your DOB
 
-if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
-  age--;
+// const today = new Date();
+// let age = today.getFullYear() - dob.getFullYear();
+// const m = today.getMonth() - dob.getMonth();
+
+// if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
+//   age--;
+// }
+
+// document.getElementById("age").textContent = age;
+
+// {
+//   /* <span id="age"></span> */
+// }
+
+// 👇 SAME format as you want
+const dobString = "01-10-1999"; // DD-MM-YYYY
+
+// manual parsing
+const [day, month, year] = dobString.split("-");
+const dob = new Date(year, month - 1, day); // JS safe date
+
+function updateAge() {
+  const today = new Date();
+  let age = today.getFullYear() - dob.getFullYear();
+
+  if (
+    today.getMonth() < dob.getMonth() ||
+    (today.getMonth() === dob.getMonth() && today.getDate() < dob.getDate())
+  ) {
+    age--;
+  }
+
+  document.getElementById("age").textContent = age;
 }
 
-document.getElementById("age").textContent = age;
+// initial call
+updateAge();
+
+// 🔁 auto update (birthday pe khud change ho jayega)
+setInterval(updateAge, 60 * 60 * 1000); // every 1 hour
+
+//===================== upadate date of birthday ======================//
 
 // const navLinks = document.querySelectorAll(".navmenu a");
 // const offcanvasEl = document.getElementById("offcanvasNavbar2");
@@ -63,3 +98,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+//======================= contact messgae send ======================//
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault(); // page reload rokta hai
+
+    emailjs
+      .sendForm(
+        "service_stvmlbk", // 👈 Service ID
+        "template_gfd10ug", // 👈 Template ID
+        this,
+      )
+      .then(
+        function () {
+          alert("Message sent successfully ✅");
+          document.getElementById("contact-form").reset();
+        },
+        function (error) {
+          alert("Failed to send ❌");
+          console.log(error);
+        },
+      );
+  });
